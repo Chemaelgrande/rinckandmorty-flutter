7f314bc66f126836a491efaa67e0745874994a5d
jose09511 <jose09511@gmail.com>
2022-04-02 16:40:36 +0200

    create get structure and home

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
 
-void main() => runApp(const MyApp());
+import 'app/routes/app_pages.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Image.asset("assets/images/other-image-rickandmorty.jpeg"),
-      ),
-    );
-  }
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
